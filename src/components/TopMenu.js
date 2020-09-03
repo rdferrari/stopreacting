8899588a9ccef68f5755197afd3d1942e0ebbdb4
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2020-09-03 16:50:40 +1200

    Install bt test

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -4,41 +4,21 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../Routes";
 
 const TopMenu = ({ signOut }) => {
-  const [installPromptEvent, setInstallPromptEvent] = useState();
-  useEffect(() => {
-    const beforeInstallPromptHandler = (event) => {
-      event.preventDefault();
+  // console.log(handleInstallPwa);
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
 
-      // store the event for later use
-      setInstallPromptEvent(event);
-    };
-    window.addEventListener("beforeinstallprompt", beforeInstallPromptHandler);
-    return () =>
-      window.removeEventListener(
-        "beforeinstallprompt",
-        beforeInstallPromptHandler
-      );
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      setDeferredPrompt(e);
+      // Update UI notify the user they can install the PWA
+      // showInstallPromotion();
+    });
   }, []);
 
-  //   const handleInstallDeclined = () => {
-  //     // handleUserSeeingInstallPrompt();
-  //     setInstallPromptEvent(null);
-  //   };
-
-  const handleInstallAccepted = () => {
-    console.log("Install PWA");
-    // show native prompt
-    installPromptEvent.prompt();
-
-    // decide what to do after the user chooses
-    installPromptEvent.userChoice.then((choice) => {
-      // if the user declined, we don't want to show the prompt again
-      //   if (choice.outcome !== "accepted") {
-      //     handleUserSeeingInstallPrompt();
-      //   }
-      setInstallPromptEvent(null);
-    });
-  };
+  console.log(deferredPrompt);
 
   return (
     <UserContext.Consumer>
@@ -55,7 +35,7 @@ const TopMenu = ({ signOut }) => {
             <button onClick={() => signOut}>Sign out</button>
           )}
           <div>
-            <button onClick={handleInstallAccepted}>Install</button>
+            <button onClick={() => deferredPrompt.prompt()}>Install</button>
           </div>
         </div>
       )}
